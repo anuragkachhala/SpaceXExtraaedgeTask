94af20e11296bb61ee63fe4cea6ef4f880dfe0f7
Anurag Kachhala <kachhalaanurag@gmail.com>
2022-01-29 11:17:24 +0530

    buildSrc added for dependencies

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,38 @@
+import Version.activity_ktx_version
+import Version.appcompat_version
+import Version.constraint_layout_version
+import Version.core_ktx_version
+import Version.gradle_version
+import Version.kotlin_gradle_version
+import Version.material_version
+
+/**
+ * @Author: Anurag Kumar Kachhala
+ * @Date: 29,January,2022
+ */
+
+object AppDependencies {
+    const val gradle = "com.android.tools.build:gradle:${gradle_version}"
+    const val kotlin = "org.jetbrains.kotlin:kotlin-stdlib:1.4.21-2"
+    const val kotlinGradle =
+        "org.jetbrains.kotlin:kotlin-gradle-plugin:${kotlin_gradle_version}"
+}
+
+
+// core
+object AndroidCore {
+    const val coreKtx = "androidx.core:core-ktx:${core_ktx_version}"
+    const val appcompat = "androidx.appcompat:appcompat:${appcompat_version}"
+    const val materialDesign = "com.google.android.material:material:${material_version}"
+    const val constraintLayout =
+        "androidx.constraintlayout:constraintlayout:${constraint_layout_version}"
+    const val activityKtx = "androidx.activity:activity-ktx:${activity_ktx_version}"
+}
+
+
+//Testing
+object Testing {
+    const val jUnit = "junit:junit:${Version.junit}"
+    const val extJUnit = "androidx.test.ext:junit:${Version.test_junit}"
+    const val espresso = "androidx.test.espresso:espresso-core:${Version.espresso}"
+}
---
+++ buildSrc/src/main/java/Version.kt
@@ -0,0 +1,33 @@
+/**
+ * @Author: Anurag Kumar Kachhala
+ * @Date: 29,January,2022
+ */
+
+object Version {
+
+    const val compileSdk = 31
+    const val minSdk = 21
+    const val targetSdk = 31
+    const val buildToolsVersion = "31.0.0"
+    const val versionCode= 1
+    const val versionName = "1.0"
+
+    //app
+    const val gradle_version ="7.0.4"
+    const val kotlin_gradle_version="1.6.0"
+
+    //core
+    const val core_ktx_version = "1.7.0"
+    const val appcompat_version = "1.4.1"
+    const val material_version = "1.5.0"
+    const val constraint_layout_version = "2.1.3"
+    const val activity_ktx_version = "1.4.0"
+
+
+    // test
+    const val junit = "4.13.2"
+    const val test = "1.4.0"
+    const val test_junit = "1.1.3"
+    const val espresso = "3.4.0"
+
+}
